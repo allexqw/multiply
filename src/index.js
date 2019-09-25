d5e4f67ab888f4571498e1a203ebad00b0165e78
Alexandr Hotyan <alexhotyan@gmail.com>
2019-09-25 19:08:21 +0300

    feet: multiplication of large numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let  multi=(BigInt(first))*(BigInt(second));
+  let result="";
+  result=result+multi;
+  return result;
 }
